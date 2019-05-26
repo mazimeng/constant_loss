3699e6b04d400f1dae3a4d0efc2f3d1f33920fcf
mazimeng <mazimeng@hotmail.com>
2019-05-26 23:21:53 +0800

    update

--- client.py
+++ client.py
@@ -2,17 +2,30 @@ import asyncio
 import gzip
 import json
 import time
+import uuid
 
 import websockets
 import socket
 
 
+def req(ws):
+    await ws.send(json.dumps({
+        "req": "market.{}.kline.{}".format("btcusdt", "1min"),
+        "id": str(uuid.uuid4())}))
+
+    response = await ws.recv()
+    result = gzip.decompress(response).decode('utf-8')
+    print("{}".format(result))
+
+
 async def hello():
     async with websockets.connect("wss://api.huobi.pro/ws") as websocket:
-        await websocket.send(json.dumps({"ping": time.time()}))
+        # await websocket.send(json.dumps({"ping": time.time()}))
+        #
+        # response = await websocket.recv()
+        # result = gzip.decompress(response).decode('utf-8')
+        # print("{}".format(result))
+        req(websocket)
 
-        response = await websocket.recv()
-        result = gzip.decompress(response).decode('utf-8')
-        print("{}".format(result))
 
 asyncio.get_event_loop().run_until_complete(hello())
